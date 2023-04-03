4350cc0ca8c942ed0daebbfdb21fb54d975be3d9
TabMacedo1897 <tabata.macedo1897@gmail.com>
2023-04-03 09:59:38 -0300

    instalação do firebase

---
+++ PRO_1-1_C71_AtividadeDaProfessora4-main/config.js
@@ -0,0 +1,16 @@
+// require("@firebase/firestore");
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdTFPsEJoDDJOu8cxh1rK6z0pTe4QyB7o",
+  authDomain: "biblioteca-eletronica-27177.firebaseapp.com",
+  projectId: "biblioteca-eletronica-27177",
+  storageBucket: "biblioteca-eletronica-27177.appspot.com",
+  messagingSenderId: "601499718101",
+  appId: "1:601499718101:web:0fdd1cbe2ce9bead58ed5c",
+  measurementId: "G-NF498E9XMM",
+};
+
+// firebase.initializeApp(firebaseConfig);
+
+export default batata = "";
